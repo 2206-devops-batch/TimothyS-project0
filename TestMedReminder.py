4e9d14be00103402036568b1dd094946cfb81f9e
TSim77 <TimSim777@gmail.com>
2022-06-22 01:10:00 -0500

    Changed test case

--- TestMedReminder.py
+++ TestMedReminder.py
@@ -1,16 +1,31 @@
+import random
 import unittest
-from Clients.SampleClientMedList import MedList
 import Medications
+import MedReminder
+from Clients.TimothyMedList import MedList as test_meds
 
 
 class TestEndcodeImage(unittest.TestCase):
 
-    def test_get_monday_meds(self):
-        self.assertDictEqual(
-            MedList["MONDAY"]["1200"], {"Pill C"})
+    # def test_get_monday_meds(self):
+    #     self.assertDictEqual(
+    #         MedList["MONDAY"]["1200"], {"Pill C"})
 
-    def test_get_all_noon_and_midnight_meds(self):
-        pass
+    # def test_get_all_noon_and_midnight_meds(self):
+    #     pass
+
+    def test_get_meds_today(self):
+        print("Med List Test: Get Meds Today \t\t\t| Started")
+        today = random.choice(Medications["days_of_week"])
+        for hour in range(24):
+            hour_24 = "0" + str(hour) + "00" if hour < 10 else str(hour) + "00"
+
+        # Test Case
+        expected_test_data = test_meds[today]
+        our_method_results = Medications.get_meds(test_meds, today, hour_24)
+        error_message = f'Error Result are not what we wantExpected: {expected_test_data} | Actual: {our_method_results}'
+        self.assertDictEqual(our_method_results,
+                             expected_test_data, error_message)
 
 
 if __name__ == '__main__':
